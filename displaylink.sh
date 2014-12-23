11206bc50d63a25a7932abee12fd25650d7e7321
Nathan Typanski <ntypanski@gmail.com>
2014-12-23 15:56:03 -0500

    license year bump: 2013-2014 now

--- displaylink.sh
+++ displaylink.sh
@@ -2,7 +2,7 @@
 #
 # The MIT License (MIT)
 #
-# Copyright (c) 2013 Nathan Typanski
+# Copyright (c) 2013-2014 Nathan Typanski
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
